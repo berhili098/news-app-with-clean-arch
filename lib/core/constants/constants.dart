4ef4698d888ce1e3ce0afaef2b2515626bbfb27a
oussama berhili <oussamaberhili190@gmail.com>
2025-02-17 16:28:54 +0100

    Add initial project structure and configuration files for Flutter application

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2/';
+const String newsApiKey = '48f7511b53064bc7b260d58ef0f8ef7a';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
---
+++ lib/features/daily_news/data/models/article_model.dart
@@ -0,0 +1,40 @@
+import 'package:cleanarch/features/daily_news/domain/entities/article_entity.dart';
+
+class ArticleModel extends ArticleEntity {
+  const ArticleModel({
+    required super.id,
+    super.author,
+    super.title,
+    super.description,
+    super.url,
+    super.urlToImage,
+    super.publishedAt,
+    super.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      id: json['id'],
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: json['publishedAt'],
+      content: json['content'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt,
+      'content': content,
+    };
+  }
+}
